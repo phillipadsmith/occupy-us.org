4bde0832422e6cdb4382a93d85af9540bfd54d74
Phillip Smith <ps@phillipadsmith.com>
2012-12-02 22:28:52 -0600

    Limited front page to 4 posts for Issue No 2. Need a better solution

--- wp-content/themes/occupy-us/index.php
+++ wp-content/themes/occupy-us/index.php
@@ -16,7 +16,7 @@ get_header(); ?>
     <?php
               // Blog post query
     $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-    query_posts( array( 'post_type' => 'post', 'paged'=>$paged, 'showposts'=>0) );
+    query_posts( array( 'post_type' => 'post', 'paged'=>$paged, 'showposts'=>4) );
     if (have_posts()) : while ( have_posts() ) : the_post(); ?>
     <div <?php post_class(); ?>>
       <div class="row">
@@ -25,7 +25,12 @@ get_header(); ?>
         <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" >
           <?php the_post_thumbnail('full');?></a>
       <a href="<?php the_permalink(); ?>" title="<?php the_title();?>"><h3><?php the_title();?></h3></a>
-      <p class="meta"><?php echo bootstrapwp_posted_on();?></p>
+      <p class="meta"><?php echo occupy_posted_on();?> <span class="byline"> <span class="sep"> by </span> <?php if ( function_exists( 'coauthors_posts_links' ) ) {
+            coauthors_posts_links();
+        } else {
+            the_author_posts_link();
+        } ?></span></span>
+      </p>
 
          <?php the_excerpt();?>
        </div><!-- /.span6 -->
