09439e89b039852bc4a11fea301e931bed847111
Phillip Smith <ps@phillipadsmith.com>
2012-12-02 22:38:27 -0600

    Fix

--- wp-content/themes/occupy-us/footer.php
+++ wp-content/themes/occupy-us/footer.php
@@ -14,7 +14,8 @@
 <div class="container">
     <div class="span8 offset2">
       <p class="pull-right"><a href="#">Back to top</a></p>
-        <p>&copy; <?php bloginfo('name'); ?> <?php the_time('Y') ?>. All views expressed are those of their authors and do not represent the views of Occupy America.</p>
+        <p>&copy; <?php bloginfo('name'); ?> <?php the_time('Y') ?>
+        <br /> All views expressed are those of their authors and do not represent the views of Occupy America.</p>
           <?php
     if ( function_exists('dynamic_sidebar')) dynamic_sidebar("footer-content");
 ?>
